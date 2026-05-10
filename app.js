94e99270011a7a50b9cc43449b22a2a012b5f4d3
carlosedduDev <carrlos345@gmail.com>
2026-05-10 18:59:44 -0300

    feat: stopwatch motor and time formatting and minor change to index.html

---
+++ app.js
@@ -0,0 +1,46 @@
+const botao = document.querySelector("#btnIniciar");
+const display = document.querySelector("#tempoDisplay");
+
+// Variáveis do cronômetro
+let minutos = 25;
+let segundos = 0;
+let cronometroId = null; 
+
+// função cronômetro
+const iniciarCronometro = () => {
+
+    // verifica se o cronômetro já foi iniciado
+    if(cronometroId !== null) return;
+
+    // API assíncrona (temporizador do browser)
+    cronometroId = setInterval(() => {
+    // verifica se minutos e segundos são 0 
+    if(segundos === 0 && minutos === 0){
+        minutos += 25
+        segundos = 0
+    }else if( segundos === 0){
+        minutos -= 1
+        segundos += 59
+    }else{
+        segundos -= 1
+    }
+
+    // variáveis que recebem minutos e segundos formatados
+    let minutosFormatados = minutos.toString().padStart(2, "0");
+    let segundosFormatados = segundos.toString().padStart(2, "0");
+
+    // renderiza o cronômetro
+    display.textContent = `${minutosFormatados}:${segundosFormatados}`;
+
+}, 1000);
+}
+
+// evento de partida
+botao.addEventListener("click", () => {
+    botao.textContent = "Ativo! ⚡";
+    display.style.color = "#61afef";
+
+    // chamada da função
+    iniciarCronometro();
+
+});
